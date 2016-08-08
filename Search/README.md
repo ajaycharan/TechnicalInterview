404a59e9f35a359d0fe290d86d544b6e7fde2074
Nishanth Koganti <buntyke@gmail.com>
2016-08-08 12:23:16 +0900

    [minor] learn about binary search algorithm

--- Search/README.md
+++ Search/README.md
@@ -1,1 +1,9 @@
 ## Searching and Sorting
+
+* Given an array with elements that are sorted in increasing order, a common task to search for a particular element in the array. Serially comparing with each element would lead to a time complexity of O(n) which is not useful for large datasets.
+* An efficient searching algorithm is the binary search where we keep dividing the problem in half each time. This leads to a time complexity of $$O(log_2(n)+1)$$. Usually CS has log with base 2 and so its safe to assume the time complexity to be O(log(n)).
+* The implementation of binary search was difficult. The key idea to the solution is to use 3 variables min,mid and max to keep track of the current search area. This leads to an efficient solution. Check the [code](binarysearch.py).
+
+### Practice Problems
+
+* [binarysearch.py](binarysearch.py): Implementation of binary search algorithm.
---
+++ Search/binarysearch.py
@@ -0,0 +1,37 @@
+"""You're going to write a binary search function.
+You should use an iterative approach - meaning
+using loops.
+Your function should take two inputs:
+a Python list to search through, and the value
+you're searching for.
+Assume the list only has distinct elements,
+meaning there are no repeated values, and
+elements are in a strictly increasing order.
+Return the index of value, or -1 if the value
+doesn't exist in the list."""
+
+def binary_search(input_array, value):
+    """Your code goes here."""
+
+    N = len(input_array)
+
+    min = 0
+    max = N-1
+    mid = (N-1)/2
+
+    while mid >= 0 and mid < N-1:
+        if input_array[mid] == value:
+            return mid
+        elif input_array[mid] < value:
+            min = mid+1
+            mid = (min+max)/2
+        elif input_array[mid] > value:
+            max = mid-1
+            mid = (min+max)/2
+    return -1
+
+test_list = [1,3,9,11,15,19,29]
+test_val1 = 25
+test_val2 = 15
+print binary_search(test_list, test_val1)
+print binary_search(test_list, test_val2)
